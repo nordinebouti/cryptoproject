ea5200e1d74afd692c4f23d00590b08150513124
bouti_n <bouti_n@a44f5d2a-808a-432f-9236-e28cf0c52e50>
2016-08-12 14:49:08 +0000

    modif dir

--- src/generate_decrypt.php
+++ src/generate_decrypt.php
@@ -4,23 +4,34 @@
 	{
 		$i = 0;
 		$j = $n;
-		$temp = "";
 		$tab = array();
+		$result = array();
+		$temp = "";
+		$bin = "";
+		
 		while ($i < $n)
+		{
 			$temp .= "0";
-		$i = 0;
+			$i++;
+		}
 		foreach ($encrypt as $key => $val)
 		{
+			$i = 0;
 			$temp2 = $temp;
+			$j = $n;
 			while ($val > 0)
 				{
-					if ($val > $s2[$j]);
-						$val = $val - $s2[$n];
-					$temp2[$i] == '1';
+					if ($val > $s2[$j])
+					{
+						$val = $val - $s2[$j];
+						$temp2[$i] == '1';
+					}
+					++$i;
+					--$j;
 				}
 			array_push($temp2, $tab);
 		}
 		
-		$encrypt = implode(' ', $tab);
+		$bin = implode('', $tab);
 		return(0);
 	}
